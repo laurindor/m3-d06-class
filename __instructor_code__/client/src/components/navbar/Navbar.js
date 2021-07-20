e349eba8bd230c74be0c1a79295da9e53e6b5f5a
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-07-20 17:37:13 +0200

    done auth and protected routes

--- __instructor_code__/client/src/components/navbar/Navbar.js
+++ __instructor_code__/client/src/components/navbar/Navbar.js
@@ -19,9 +19,9 @@ function Navbar(props) {
         <li>
           <Link to="/login">Login</Link>
         </li>
-        {
-          props.loggedInUser.username && <li>Hello, {props.loggedInUser.username}</li>
-        }
+        <li>
+          <Link to="/projects/protected">Protected page</Link>
+        </li>
       </ul>
     </nav>
   )
